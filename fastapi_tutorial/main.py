d3c4af635ae59ec7e4dd01f1d95495a78b42be73
n0061q <95093640+n0061q@users.noreply.github.com>
2023-10-03 15:29:49 +0300

    Query parameters

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -5,33 +5,34 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+fake_items_db = [{"item_name": f"item-{i}"} for i in range(10)]
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/item/")
+async def get_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
 
 @app.get("/item/{item_id}")
-async def get_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(
+    item_id: str, needy: str, q: str | None = None, short: bool = False
+):
+    data = {"item_id": item_id, "needy": needy}
+    if q:
+        data.update(q=q)
+    if not short:
+        data.update(description="This is an amazing item that has a long description")
+    return data
 
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-
-@app.get("/files/{file_path:path}")
-async def get_file_path(file_path: str):
-    return {"file_path": file_path}
+@app.get("/user/{user_id}/item/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    """Multiple path and query parameters (order is not important)."""
+    data = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        data.update(q=q)
+    if not short:
+        data.update(description="This is an amazing item that has a long description")
+    return data

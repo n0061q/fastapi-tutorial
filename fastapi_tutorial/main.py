e1a528881ef3b33f4fe177281896907b4e710a7a
n0061q <95093640+n0061q@users.noreply.github.com>
2023-10-04 12:13:42 +0300

    Path Parameters and Numeric Validations

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -1,24 +1,17 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 
 app = FastAPI()
 
 
-@app.get("/items/")
-async def get_items(
-    q: Annotated[
-        str | None,
-        Query(
-            title="Query string",
-            description="Query string for the items to search in the database that have a good match",
-            alias="query",
-            # deprecated=True,
-            # include_in_schema=False,
-        ),
-    ]
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=1, lt=10)],
+    size: Annotated[float, Query(gt=0, le=2.5)],
+    q: Annotated[str | None, Query(min_length=3)] = None,
 ):
-    data = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    results = {"item_id": item_id, "size": size}
     if q:
-        data.update({"q": q})
-    return data
+        results.update({"q": q})
+    return results

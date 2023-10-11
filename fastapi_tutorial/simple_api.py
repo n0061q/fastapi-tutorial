3ef742234c5df7b83ee60258ad4c0f9a0742be93
n0061q <95093640+n0061q@users.noreply.github.com>
2023-10-11 16:23:45 +0300

    add update_hero endpoint

--- fastapi_tutorial/simple_api.py
+++ fastapi_tutorial/simple_api.py
@@ -30,6 +30,12 @@ class HeroRead(HeroBase):
     id: int
 
 
+class HeroUpdate(SQLModel):
+    name: str | None = None
+    age: int | None = None
+    secret_name: str | None = None
+
+
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
@@ -66,3 +72,22 @@ def read_hero(hero_id: int):
         if not hero:
             raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Hero not found")
         return hero
+
+
+@app.patch("/heroes/{hero_id}", response_model=HeroRead)
+def update_hero(hero_id: int, hero_update: HeroUpdate):
+    with Session(engine) as s:
+        hero = s.get(Hero, hero_id)
+        if not hero:
+            raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Hero not found")
+
+        hero_update_dict = hero_update.dict(exclude_unset=True)
+        for key, value in hero_update_dict.items():
+            setattr(hero, key, value)
+
+        s.add(hero)
+        s.commit()
+        s.refresh(hero)
+
+        return hero
+

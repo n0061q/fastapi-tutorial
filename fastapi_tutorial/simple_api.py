7afdb76a9d0abada35ac4a206cab143b788aeec5
n0061q <95093640+n0061q@users.noreply.github.com>
2023-10-16 12:51:12 +0300

    DBSession as dependency

--- fastapi_tutorial/simple_api.py
+++ fastapi_tutorial/simple_api.py
@@ -1,7 +1,8 @@
+from typing import Annotated
 from pathlib import Path
 
 from sqlmodel import Field, Session, SQLModel, create_engine, select
-from fastapi import FastAPI, HTTPException, status, Query
+from fastapi import FastAPI, HTTPException, status, Query, Depends
 
 
 DB_FILE_NAME = "database.db"
@@ -43,6 +44,14 @@ def create_db_and_tables():
 app = FastAPI()
 
 
+def get_db_session():
+    with Session(engine) as session:
+        yield session
+
+
+DBSession = Annotated[Session, Depends(get_db_session)]
+
+
 @app.on_event("startup")
 def on_startup():
     # Path(DB_FILE_NAME).unlink(missing_ok=True)
@@ -50,55 +59,50 @@ def on_startup():
 
 
 @app.post("/heroes/", response_model=HeroRead)
-def create_hero(hero: HeroCreate):
-    with Session(engine) as s:
-        db_hero = Hero.from_orm(hero)
-        s.add(db_hero)
-        s.commit()
-        s.refresh(db_hero)
-        return db_hero
+def create_hero(session: DBSession, hero: HeroCreate):
+    db_hero = Hero.from_orm(hero)
+    session.add(db_hero)
+    session.commit()
+    session.refresh(db_hero)
+    return db_hero
 
 
 @app.get("/heroes/", response_model=list[HeroRead])
-def read_heroes(offset: int = 0, limit: int = Query(default=100, le=100)):
-    with Session(engine) as s:
-        return s.exec(select(Hero).offset(offset).limit(limit)).all()
+def read_heroes(session: DBSession, offset: int = 0, limit: int = Query(default=100, le=100)):
+    return session.exec(select(Hero).offset(offset).limit(limit)).all()
 
 
 @app.get("/heroes/{hero_id}", response_model=HeroRead)
-def read_hero(hero_id: int):
-    with Session(engine) as s:
-        hero = s.get(Hero, hero_id)
-        if not hero:
-            raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Hero not found")
-        return hero
+def read_hero(session: DBSession, hero_id: int):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Hero not found")
+    return hero
 
 
 @app.patch("/heroes/{hero_id}", response_model=HeroRead)
-def update_hero(hero_id: int, hero_update: HeroUpdate):
-    with Session(engine) as s:
-        hero = s.get(Hero, hero_id)
-        if not hero:
-            raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Hero not found")
+def update_hero(session: DBSession, hero_id: int, hero_update: HeroUpdate):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Hero not found")
 
-        hero_update_dict = hero_update.dict(exclude_unset=True)
-        for key, value in hero_update_dict.items():
-            setattr(hero, key, value)
+    hero_update_dict = hero_update.dict(exclude_unset=True)
+    for key, value in hero_update_dict.items():
+        setattr(hero, key, value)
 
-        s.add(hero)
-        s.commit()
-        s.refresh(hero)
+    session.add(hero)
+    session.commit()
+    session.refresh(hero)
 
-        return hero
+    return hero
 
 
 @app.delete("/heroes/{hero_id}")
-def delete_hero(hero_id: int):
-    with Session(engine) as s:
-        hero = s.get(Hero, hero_id)
-        if not hero:
-            raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Hero not found")
+def delete_hero(session: DBSession, hero_id: int):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Hero not found")
 
-        s.delete(hero)
-        s.commit()
-        return {"ok": True}
+    session.delete(hero)
+    session.commit()
+    return {"ok": True}
